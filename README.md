2426ac6df633cb8c90f09456bde60652ba8bf3d5
Stanislas Rigal <stanislas.rigal@ens-lyon.fr>
2022-05-30 11:09:18 +0200

    Spatial associations.

--- README.md
+++ README.md
@@ -22,10 +22,53 @@ source("R_packages.R")
 
 # Load data from the French Breeding Bird Survey (STOC-EPS)
 
-dataprp <- readRDS("output/dataprp.rds")
+dataprp <- readRDS("raw_data/dataprp.rds")
 
 # Load geographical information on sites
 
-square_centroid <- readRDS("output/square_centroid.rds")
+square_centroid <- readRDS("raw_data/square_centroid.rds")
 
 ```
+
+## Spatial species associations
+
+Details on the calculation of species associations from spatial data are available in Rigal et al. (2022). The difference here is that spatial associations are not calculated for each year but for the entire period (2001-2017).
+
+### Loading functions
+
+```{r setup, include=FALSE}
+
+# Load functions for spatial associations
+
+source("function_spatial_associations.R")
+
+```
+
+
+### Calculating species associations
+
+Warning: it might take a while.
+
+```{r}
+registerDoParallel(cores = c(detectCores()-1)) # parallelise computation as this might take a while
+
+dataperpoint <- dcast(dataprp,code_point+habit+zonebio~code_sp,fun.aggregate = sum,value.var="abond") # use data from all years
+
+spatial_association <- ddply(dataperpoint, .(habit, zonebio), .fun=g, 
+                           N.null=1000, occmin=0, 
+                           .parallel = TRUE)
+
+spatial_association$ses_init <- spatial_association$spatial_asso
+
+spatial_association$spatial_asso[spatial_association$pval>0.05] <- NA
+
+```
+
+
+
+
+## References
+
+https://doi.org/10.1111/ecog.01892
+
+Rigal, S., Devictor, V., Gaüzère, P., Kéfi, S., Forsman, J. T., Kajanus, M. H., ... & Dakos, V. (2022). Biotic homogenisation in bird communities leads to large‐scale changes in species associations. Oikos, 2022(3), e08756. https://doi.org/10.1111/oik.08756
---
+++ function_spatial_associations.R
@@ -0,0 +1,109 @@
+# Function to randomise communities
+# Based on `make_netassoc_network` (Morueta-Holmes et al., 2016). The idea is that a species enters a community with a probability that is proportional to its abundance in the regional pool (the whole dataset).
+
+randomizeM <- function(mat) { 
+  # Compute total species frequencies
+  total_freqs <- apply(mat, 2, sum)
+  total_freqs <- total_freqs / sum(total_freqs)
+  result <- apply(mat, 1, function(X) { 
+    # We sample sum(X) individuals from the regional pool. With the probability 
+    # of a species entering the community proportional to its abundance in the 
+    # pool (the whole dataset)
+    picked_species <- sample(seq.int(ncol(mat)), 
+                             replace = TRUE, 
+                             size = sum(X),
+                             prob = total_freqs)
+    tabulate(picked_species, nbins = ncol(mat))
+  })
+  t(result) 
+}
+
+# Function used to present results
+
+```{r}
+tabularize <- function(coocmat, name = "assoc") { 
+  # Zero-size matrix
+  if(is.null(dim(coocmat))){return(NA)}
+  # Add numbers if species names are absent in the original matrix
+  if(is.null(colnames(coocmat))){ 
+    spnames <- seq.int(ncol(coocmat))
+  } else { 
+    spnames <- colnames(coocmat)
+  }
+  # Create new table
+  tab <- data.frame(expand.grid(spi = spnames, spj = spnames), 
+                    as.vector(coocmat))
+  names(tab) <- c('spi', 'spj', name)
+  return(tab)
+}
+
+# Function to calculate species associations
+
+```{r}
+
+g < -function(mat, # observed data
+            N.null, # number of null matrices to simulate
+            occmin){ # minimum occurence number for a species to be selected
+  
+  mat[,c("code_point","year","habit","zonebio")] <- list(NULL)
+  
+  mat <- round(mat)
+  
+  # select species with more occurence than occmin
+  matpa <- replace(mat, mat != 0, 1)
+  mat <- mat[, colSums(matpa) > occmin]
+  name <- names(mat)
+  
+  # log-transformation of abundance data as recommanded in Morueta-Holmes et al. 2016 (https://doi.org/10.1111/ecog.01892)
+  mat <- log(mat+1e-6) - log(1e-6)
+  
+  # observed association matrix
+  mat <- t(as.matrix(mat))
+  D.obs <- partial_correlation(mat,"shrinkage")
+  
+  # random association matrices
+  D.null <- array(rep(NA,nrow(mat)*nrow(mat)*N.null),
+                  dim = c(nrow(mat),nrow(mat),N.null))
+  
+  for(null in 1:N.null) {
+    D.null[ , ,null] <- partial_correlation(apply(replicate(1000, t(randomizeM(t(mat)))), c(1, 2), mean),
+                                            "shrinkage")
+  }
+  
+  D.null.mean <- apply(D.null, c(1,2), mean)
+  D.null.sd <- apply(D.null, c(1,2), sd)
+  
+  SES <- (D.obs-D.null.mean)/D.null.sd
+  
+  # pvalue matrix
+  M <- array(c(D.obs, D.null), dim=c(nrow(mat),nrow(mat),N.null+1))
+  p.val <- apply(M, c(1,2), function(x) {
+    if ( (rank(x)[1]/(N.null+1)) < 0.5 ) { 
+      rank(x)[1]/(N.null+1)
+    } else { 
+      ( 1 - rank(x)[1]/(N.null+1) )
+    } 
+  }) 
+  
+  p.val <- 2*p.val
+  diag(p.val) <- NA
+  
+  # adjusted pvalues
+  p.val.adj <- matrix(p.adjust(p.val, method = "BH"), nrow=nrow(p.val), ncol=ncol(p.val))
+  
+  # select significant SES
+  SES[p.val.adj>0.05] <- NA
+  SES <- matrix(SES, nrow = nrow(p.val.adj), ncol = ncol(p.val.adj))
+  
+  # retrieve selected species names
+  attr(SES,"dimnames") <- list(name)
+  attr(SES,"dimnames")[[2]] <- attr(SES,"dimnames")[[1]]
+  
+  results <- data.frame(tabularize(D.obs, name = "obs"), 
+                        spatial_asso = tabularize(SES)[ ,3], 
+                        pval = tabularize(p.val.adj)[ ,3], 
+                        nullmean = tabularize(D.null.mean)[ ,3], 
+                        nullsd = tabularize(D.null.sd)[ ,3])
+  
+  return(results)
+}
