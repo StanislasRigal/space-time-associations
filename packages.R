be1abcd1c08a6c3dee46a693161a70a31ec6424f
Stanislas Rigal <stanislas.rigal@ens-lyon.fr>
2025-07-22 16:27:08 +0200

    Update R script for figure.

--- packages.R
+++ packages.R
@@ -1,41 +1,47 @@
 # Load packages
 
-library(reshape2)
-library(plyr)
-library(dplyr)
-library(lattice)
-library(ggplot2)
-library(compare)
-library(nlme)
-library(classInt)
-library(maptools)
-library(stringr)
-library(rgeos)
-library(rgdal)
-library(sp)
-library(mgcv)
-library(data.table)
-library(automap)
-library(glmnet)
-library(raster)
-library(viridis)
-library(see)
-library(netassoc)
-library(picante)
-library(doParallel)
-library(igraph)
-library(ggraph)
-library(spgwr)
-library(entropart)
-library(rEDM)
-library(pforeach)
-library(multispatialCCM)
-library(ade4)
-library(RVAideMemoire)
-library(cluster)
-library(geometry)
-library(ape) 
-library(phangorn)
-library(quantreg)
-library(broom)
-library(piecewiseSEM)
+library(reshape2) # 1.4.4
+library(plyr) # 1.8.8
+library(dplyr) # 1.0.10
+library(lattice) # 0.20-45
+library(ggplot2) # 3.5.1
+library(compare) # 0.2-6
+library(nlme) # 3.1-160
+library(classInt) # 0.4-8
+#library(maptools)
+library(stringr) # 1.5.0
+#library(rgeos)
+#library(rgdal)
+library(sp) # 1.5-1
+library(mgcv) # 1.8-41
+library(data.table) # 1.14-6
+library(automap) # 1.0-16
+library(glmnet) # 4.1-6
+library(raster) # 3.6-11
+library(viridis) # 0.6.2
+library(see) # 0.7.4
+library(netassoc) # 0.7.0
+library(picante) # 1.8.2
+library(doParallel) # 1.0.17
+library(igraph) # 1.3.5
+library(ggraph) # 2.1.0
+library(spgwr) # 0.6-35
+library(entropart) # 1.6-11
+library(rEDM) # 1.13.1
+#library(pforeach)
+library(multispatialCCM) # 1.2
+library(ade4) # 1.7-20
+library(RVAideMemoire) # 0.9-81-2
+library(cluster) # 2.1.4
+library(geometry) # 0.4.6.1
+library(ape) # 5.6-2
+library(phangorn) # 2.11.1
+library(quantreg) # 5.94
+library(broom) # 1.0.2
+library(piecewiseSEM) # 2.3.0
+library(rnetcarto) # 0.2.6
+library(clue) # 0.3-65
+library(boot) # 1.3-28
+library(network) # 1.18.0
+library(ggnet) # 0.1.0
+library(egg) # 0.4.5
